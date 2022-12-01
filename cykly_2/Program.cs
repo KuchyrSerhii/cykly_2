4f1286d1dfd1a4c193dabb5594eafc42479f98a5
ou4pc18 <ou4pc18@OU4PC18>
2022-12-01 11:13:57 +0100

    Přidejte soubory projektu.

---
+++ cykly_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int body = 0;
+Console.WriteLine("Je Slunce koulí žhavých plynů?");
+string spravna_odpoved = "ano";
+string odpoved = Console.ReadLine();
+if (odpoved.ToLower() == spravna_odpoved.ToLower())
+{
+    Console.WriteLine("Spavně! Předtím jsi měl {0} bodů", ++body);
+}
+
+
